d71e9f3ab8ff7a4e80d373786008ce3f7a6e083d
SuhanVerse <khsuhan100@gmail.com>
2026-02-16 13:37:30 +0545

    fix: added form to edit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,8 +2,14 @@ import { useState } from "react";
 import { toast } from "react-toastify";
 import "./Form.css";
 
-const Form = ({ addItem }) => {
-    const [newItemName, setNewItemName] = useState("");
+const Form = ({
+    addItem,
+    editItemId,
+    updateItemName,
+    itemToEdit,
+    inputRef,
+}) => {
+    const [newItemName, setNewItemName] = useState(itemToEdit?.name || "");
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -11,23 +17,28 @@ const Form = ({ addItem }) => {
             toast.error("please provide value");
             return;
         }
-        addItem(newItemName);
+        if (editItemId) {
+            updateItemName(newItemName);
+        } else {
+            addItem(newItemName);
+        }
         setNewItemName("");
     };
 
     return (
         <form onSubmit={handleSubmit}>
-            <h2>grocery bud</h2>
+            <h2>Grocery bud</h2>
             <div className="form-control">
                 <input
                     type="text"
                     className="form-input"
-                    value={newItemName}
+                    value={itemToEdit?.name || newItemName}
+                    ref={inputRef}
                     placeholder="e.g. eggs"
                     onChange={(event) => setNewItemName(event.target.value)}
                 />
                 <button type="submit" className="btn">
-                    add item
+                    {editItemId ? "edit item" : "add item"}
                 </button>
             </div>
         </form>

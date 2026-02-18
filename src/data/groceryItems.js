94dd86fbc2038061ab7031234a45952ac817fbbc
SuhanVerse <khsuhan100@gmail.com>
2026-02-18 21:44:47 +0545

    refactor: Remove sample data

--- src/data/groceryItems.js
+++ src/data/groceryItems.js
@@ -1,8 +1,8 @@
 // groceryItems.js
 
-export const groceryItems = [
-    { id: "1", name: "milk", completed: true },
-    { id: "2", name: "bread", completed: true },
-    { id: "3", name: "eggs", completed: false },
-    { id: "4", name: "butter", completed: false },
-];
+// export const groceryItems = [
+//     { id: "1", name: "milk", completed: true },
+//     { id: "2", name: "bread", completed: true },
+//     { id: "3", name: "eggs", completed: false },
+//     { id: "4", name: "butter", completed: false },
+// ];
